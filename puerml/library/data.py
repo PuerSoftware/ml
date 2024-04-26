611df17b9c1d914ad3971972a6d58059e6cb6a6e
Alexander Podgorny <apodgorny@users.noreply.github.com>
2024-04-27 01:56:54 +0300

    Broke all tests

--- puerml/library/data.py
+++ puerml/library/data.py
@@ -1,185 +1,200 @@
-import os
-import shutil
-import io
-import json
-import zipfile
-import requests
+import magic
 
-__all__ = ['Data']
+'''
+Single file
+mypackage.jsonl
 
+Multiple files
+mypackage.jsonl/0
+mypackage.jsonl/1
 
-class Data:
-	def __init__(self, data=None, file_type=None, is_binary=False):
-		self.data       = io.BytesIO(data) if is_binary else io.StringIO(data)
-		self.is_binary  = is_binary
-		self.file_type  = file_type if data else None
-		self.headers    = None
-		self.location   = None
-		self.from_web   = None
-		self.description = ''
+Load - Save
+Data.load('mypackage.jsonl')
+Data.save('mypackage.jsonl', max_size)
+'''
 
-	def _read_manifest(self):
-		file_loc = os.path.join(self.location, 'manifest.json')
-		if self.from_web:
-			response = requests.get(file_loc, headers=self.headers)
-			return json.loads(response.text)
-		else:
-			with open(file_loc) as f:
-				return json.load(f)
+############################################################
 
-	def _write_manifest(self, location, manifest):
-		file_loc = os.path.join(location, 'manifest.json')
-		with open(file_loc, 'w') as f:
-			return json.dump(manifest, f)
+class DataReader:
+	def __init__(self):
+		self._data     = None
+		self.is_binary = None
+		self.is_local  = False
 
-	def _read_web(self, file_loc):
-		response = requests.get(file_loc, headers=self.headers, stream=True)
+	def _test_content(self, content):
+		mime           = magic.Magic(mime=True)
+        file_type      = mime.from_buffer(content)
+        self.is_binary = not file_type.startswith('text/')
+
+	def _read_remote(self, location, headers):
+		response = requests.get(location, headers=headers, stream=True)
 		if response.status_code == 200:
+			if self.is_binary is None:
+				self._test_content(response.content)
+
 			return response
 		return None
 
-	def _read_disk(self, file_loc):
+	def _read_local(self, location):
 		try:
-			if os.path.exists(file_loc):
+			if os.path.exists(location):
+				if self.is_binary is None:
+					with open(location, 'rb') as f:
+						self._test_content(f.read(1024))
+
 				mode = 'rb' if self.is_binary else 'r'
-				return open(file_loc, mode)
+				return open(location, mode)
 		except Exception as e:
-			print(f'Failed to open file {file_loc}: {e}')
+			print(f'Failed to read file "{location}": {e}')
 		return None
 
-	def _file_generator(self):
-		if self.data:
-			yield self.data
+	def _read(self, location, headers=None):
+		if self.is_local:
+			return self._read_local(location)
+		return self._read_remote(location, headers)
+
+	def _file_generator(self, location, headers=None):
+		if self._data:
+			yield self._data
 		else:
-			location = f'{self.location}.{self.file_type}'
-			if os.path.exists(location):
-				with open(location, 'rb' if self.is_binary else 'r') as f:
-					yield f
-			else:
-				n = 0
-				while True:
-					file_name  = f'{n}.{self.file_type}' if self.file_type else str(n)
-					file_loc = os.path.join(self.location, file_name)
-					read = self._read_web if self.from_web else self._read_disk
-					file = read(file_loc)
+			n = 0
+			while True:
+				loc  = os.path.join(location, str(n))
+				file = self._read(loc, headers)
+				if file:
+					# reading package
+					yield file
+					if self.is_local: file.close()
+					n += 1
+				elif n == 0:
+					# Reading single file
+					file = self._read(location, headers)
 					if file:
 						yield file
-						if not self.from_web:
-							file.close()
-						n += 1
-					else:
+						if self.is_local: file.close()
 						break
-
-	def _content_generator(self):  # TODO: modify to produce chunks efficiently
-		for file in self._file_generator():
-			if self.from_web:
-				yield file.content if self.is_binary else file.text
-			else:
-				yield file.read()
-
-	def _chunk_generator(self, max_size=1024*1024):
-		for file in self._file_generator():
-			if self.from_web:
-				for chunk in file.iter_content(chunk_size=max_size):
-					yield chunk if self.is_binary else chunk.decode('utf-8')
-			else:
-				while True:
-					chunk = file.read(max_size)
-					if not chunk:
-						break
-					yield chunk
+				else:
+					break
 
 	def _line_generator(self):
+		if self.is_binary:
+			raise Exception('Can not iterate lines of binary file')
 		for file in self._file_generator():
-			iterator = file.iter_lines(decode_unicode=True) if self.from_web else file
+			iterator = file if self.is_local else file.iter_lines(decode_unicode=True)
 			for line in iterator:
 				yield line.strip()
 
-	################################################################################
+	def _chunk_generator_binary(self, max_size):
+		for f in self._file_generator():
+			if self.is_local:
+				while True:
+					b = f.read(max_size)
+					if not b:
+						break
+					yield b
+			else:
+				for b in f.iter_content(chunk_size=max_size):
+					yield b
+
+	def _chunk_generator_text(self, max_size)
+		b, b_size, n = [], 0, 0
+		for l in self._line_generator():
+			l_size = len(l.encode('utf-8'))
+			if l_size + b_size > max_size and len(b) > 0:
+				yield '\n'.join(b)
+				b, b_size, n = [], 0, n + 1
+			b.append(l)
+			b_size += l_size
+
+		if b_size > 0:
+			yield '\n'.join(b)
+
+	def _chunk_generator(self, max_size=1024**4):
+		if self.is_binary:
+			return self._chunk_generator_binary(max_size)
+		return self._chunk_generator_text(max_size)
 
 	@staticmethod
-	def load(location, file_type=None, headers=None, is_binary=False):
-		d = Data()
-		d.data     = None
-		d.location = location
-		d.headers  = headers
-		d.from_web = location.startswith('http')
+	def from_location(location):
+		reader = DataReader()
+		reader.is_local = not location.startswith('http')
+		return reader
 
-		if file_type is None:
-			manifest = d._read_manifest()
-			d.description = manifest['description']
-			d.is_binary   = manifest['is_binary']
-			d.file_type   = manifest['type'].lower() if manifest['type'] else None
-		else:
-			d.description = ''
-			d.is_binary   = is_binary
-			d.file_type   = file_type
+	@staticmethod
+	def from_data(data):
+		reader = DataReader()
+		if isinstance(data, bytes):
+			reader._data     = io.BytesIO(data)
+			reader.is_binary = True
+			return reader
+		elif isinstance(data, str):
+			reader._data     = io.StringIO(data)
+			reader.is_binary = False
+			return reader
+		raise Exception(f'Expected "str" or "bytes", got "{type(data)}"')
 
-		return d
+############################################################
 
-	def save(self, location, max_size=None, description=''):
-		if max_size is None:
-			location = f'{location}.{self.file_type}'
-			with open(location, 'wb' if self.is_binary else 'w') as f:
+class DataWriter:
+	self.__init__(self, reader):
+		self._reader = reader
+
+	def write_single_file(self, location):
+		location = f'{location}.{self.file_type}'
+			with open(location, 'wb' if self._reader.is_binary else 'w') as f:
 				f.write(next(self._file_generator()).read())
-			return
 
-		if os.path.exists(location):
-			shutil.rmtree(location)
-		os.makedirs(location)
+	def write_package(self, location, max_size):
+		n = 0
+		for chunk in self._chunk_generator(max_size):
+			loc  = os.path.join(location, str(n))
+			mode = 'wb' if self._reader.is_binary else 'w'
+			with open(loc, mode) as f:
+				f.write(chunk)
+			n += 1
 
-		def _write(l, t, n, m, c):
-			fn = f'{n}.{t}' if t else str(n)
-			l = os.path.join(l, fn)
-			with open(l, m) as f:
-				f.write(c)
+############################################################
 
-		if self.is_binary:
-			n = 0
-			for chunk in self._chunk_generator(max_size):
-				_write(location, self.file_type, n, 'wb', chunk)
-				n += 1
+class Data:
+	def __init__(self, reader):
+		self.reader = reader
+		if data:
+			reader = DataReader.from_data(data)
+
+	@staticmethod
+	def load(location):
+		return Data(DataReader.from_location(location))
+
+	def load_data(data):
+		return Data(DataReader.from_data(data))
+
+	def save(self, location, max_size=None):
+		writer = DataWriter(self.reader)
+		if max_size is None:
+			writer.write_single_file(location)
 		else:
-			n = 0
-			buffer, buffer_size = [], 0
-			for line in self._line_generator():
-				line_size        = len(line.encode('utf-8'))
-				next_buffer_size = line_size + buffer_size
-				if next_buffer_size > max_size and len(buffer) > 0:
-					_write(location, self.file_type, n, 'w', '\n'.join(buffer))
-					buffer, buffer_size = [], 0
-					n += 1
-				buffer.append(line)
-				buffer_size = next_buffer_size
-			if buffer_size > 0:
-				_write(location, self.file_type, n, 'w', '\n'.join(buffer))
-				n += 1
-
-		self._write_manifest(location, {
-			'description' : description or self.description,
-			'count'       : n,
-			'type'        : self.file_type,
-			'is_binary'   : self.is_binary
-		})
+			writer.write_package(location, max_size)
 
 	def zip(self, location):
-		self.data = io.BytesIO()
-		with zipfile.ZipFile(self.data, 'w') as f:
+		data = io.BytesIO()
+		with zipfile.ZipFile(data, 'w') as f:
 			for root, _, files in os.walk(location):
 				for file in files:
 					file_path = os.path.join(root, file)
 					f.write(file_path, os.path.relpath(file_path, start=location))
-		self.data.seek(0)
+		data.seek(0)
+		self.reader._data = data
 		return self
 
 	def unzip(self, location):
 		buffer = io.BytesIO()
-		for f in self._file_generator():
+		for f in self.reader._file_generator():
 			buffer.write(f.read())
 
 		buffer.seek(0)
 		if os.path.exists(location):
 			shutil.rmtree(location)
+
 		with zipfile.ZipFile(buffer, 'r') as f:
 			f.extractall(location)
 
@@ -187,16 +202,21 @@ class Data:
 	def lines(self):
 		return self._line_generator()
 
+	@property
 	def content(self):
 		content = ''
-		for file_content in self._content_generator(): # TODO: add binary handling
+		for file_content in self._content_generator():
 			content += file_content
 		return content
 
-	def batches(self, batch_size=10): # TODO: add line preprocessing
+	def batches(self, batch_size=10, preprocess=None):
+		if self.is_binary:
+			raise Exception('Batch processing is not supported for binary files')
 		batch = []
 		for line in self._line_generator():
 			if len(batch) < batch_size:
+				if preprocess:
+					line = preprocess(line)
 				batch.append(line)
 			else:
 				yield batch
@@ -204,5 +224,4 @@ class Data:
 		if len(batch) > 0:
 			yield batch
 
-	def chunks(self, chunk_size=1024):
-		...
+############################################################
---
+++ test.py
@@ -0,0 +1,3 @@
+import os
+
+print(os.path.join('http://google.com/dir', '0'))
